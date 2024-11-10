101abd7926df1a064ee36adf8d0ab06f090911e0
Akshay kumar shaw <akshay.8459361606@gmail.com>
2024-11-10 15:21:02 +0530

    Initial Commit

---
+++ News-IOS/News_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  News_IOSApp.swift
+//  News-IOS
+//
+//  Created by Akshay kumar shaw on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct News_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
